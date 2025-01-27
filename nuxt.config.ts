4afcdd8942b36069d05959ea91e5598065438745
shivam singh <shivam.singh494x@gmail.com>
2025-01-27 16:18:23 +0530

    nav done4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
     viewer: true,
     exposeConfig: false,
   },
+  ui: {
+    global: true
+  },
   plugins: ["~/plugins/gsap.js"],
   css: ["~/assets/css/global.css"],
   image: {
